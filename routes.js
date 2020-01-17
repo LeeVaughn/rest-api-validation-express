fc12bc0e5a81958053db1becaa4355760b478d6e
LeeVaughn <dlvprogramming@gmail.com>
2020-01-17 13:10:10 -0800

    required express-validator and used it for validations on /users POST route

--- routes.js
+++ routes.js
@@ -1,6 +1,11 @@
 'use strict';
 
 const express = require('express');
+const { check, validationResult } = require('express-validator/check');
+// Above code uses destructuring, it is the same as:
+// const checkModule = require('express-validator/check');
+// const check = checkModule.check;
+// const validationResult = checkModule.validationResult;
 
 // This array is used to keep track of user records
 // as they are created.
@@ -9,32 +14,39 @@ const users = [];
 // Construct a router instance.
 const router = express.Router();
 
+
+const nameValidationChain = check('name')
+  .exists({ checkNull: true, checkFalsy: true })
+  .withMessage('Please provide a value for "name"');
+
 // Route that returns a list of users.
 router.get('/users', (req, res) => {
   res.json(users);
 });
 
 // Route that creates a new user.
-router.post('/users', (req, res) => {
-  // Get the user from the request body.
-  const user = req.body;
-  const errors = [];
+router.post('/users', [
+  check('name')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "name"'),
+  check('email')
+    .exists()
+    .withMessage('Please provide a value for "email"'),
+], (req, res) => {
+  // Attempt to get the validation result from the Request object.
+  const errors = validationResult(req);
 
-  // Validate that we have a `name` value.
-  if (!user.name) {
-    errors.push('Please provide a value for "name"');
-  }
+  // If there are validation errors...
+  if (!errors.isEmpty()) {
+    // Use the Array `map()` method to get a list of error messages.
+    const errorMessages = errors.array().map(error => error.msg);
 
-  // Validate that we have an `email` value.
-  if (!user.email) {
-    errors.push('Please provide a value for "email"');
-  }
-
-  // If there are any errors...
-  if (errors.length > 0) {
-    // Return the validation errors to the client, same as errors: errors
-    res.status(400).json({ errors });
+    // Return the validation errors to the client.
+    res.status(400).json({ errors: errorMessages });
   } else {
+    // Get the user from the request body.
+    const user = req.body;
+
     // Add the user to the `users` array.
     users.push(user);
 
